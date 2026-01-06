a0376f489ec380009d033230f6a203415430ba14
JaninduTharuka <janindutharuka2001@gmail.com>
2026-01-06 14:49:54 +0530

    small bug

--- dataset_preprocessing/set_target.py
+++ dataset_preprocessing/set_target.py
@@ -2,7 +2,7 @@ import os
 import shutil
 import numpy as np
 
-target = "acceleration"     # displacement, velocity_difference, acceleration
+target = "accelerations"     # displacements, velocity_differences, accelerations
 dataset_ver = 6
 num_of_iterations = 100
 frames_per_iteration = 300
@@ -12,13 +12,13 @@ delta_t = 1 / fps
 dataset_path = f"../../datasets/{str(dataset_ver)}/flags"
 target_path = f"../../datasets/{str(dataset_ver)}/targets"
 
-if target == "displacement":
+if target == "displacements":
     target_path = os.path.join(target_path, "displacements")
     print("targeting displacement...")
-elif target == "velocity_difference":
+elif target == "velocity_differences":
     target_path = os.path.join(target_path, "velocity_differences")
     print("targeting velocity difference...")
-elif target == "acceleration":
+elif target == "accelerations":
     target_path = os.path.join(target_path, "accelerations")
     print("targeting acceleration...")
 
@@ -29,17 +29,17 @@ os.makedirs(target_path, exist_ok=False)
 for i in range (1, num_of_iterations+1):
     for j in range (0, frames_per_iteration):
         print(f"\r Processing iteration {i}, frame {j}...", end="", flush=True)
-        if target == "displacement":
+        if target == "displacements":
             first = np.load(dataset_path + f'/flag_{i:03d}_{j:03d}.npy')
             second = np.load(dataset_path + f'/flag_{i:03d}_{j+1:03d}.npy')
             displacement = second[:,:3] - first[:,:3]
             np.save(target_path + f'/target_{i:03d}_{j:03d}.npy', displacement)
-        elif target == "velocity_difference":
+        elif target == "velocity_differences":
             first = np.load(dataset_path + f'/flag_{i:03d}_{j:03d}.npy')
             second = np.load(dataset_path + f'/flag_{i:03d}_{j+1:03d}.npy')
             velocity_difference = second[:,3:] - first[:,3:]
             np.save(target_path + f'/target_{i:03d}_{j:03d}.npy', velocity_difference)
-        elif target == "acceleration":
+        elif target == "accelerations":
             first = np.load(dataset_path + f'/flag_{i:03d}_{j:03d}.npy')
             second = np.load(dataset_path + f'/flag_{i:03d}_{j+1:03d}.npy')
             acceleration = (second[:,3:] - first[:,3:]) / delta_t
